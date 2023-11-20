c041526326372de12747892b9631a0b04b774d8f
laizu akram <laizuakram98@gmail.com>
2023-11-20 12:45:02 +0600

    create hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from 'react'
+import './Hero.css';
+import { BsList,BsFillMortarboardFill  } from "react-icons/bs";
+import { BiSolidChevronDown } from "react-icons/bi";
+import hero_bg from '../../assets/images/hero.jpg';
+import hero_bg2 from '../../assets/images/hero 2.jpg';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+// import required modules
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            <div className="category-container">
+              <div className="category-header">
+                <h3><BsList /> CATEGORY</h3> <BiSolidChevronDown />
+              </div>
+              <div className="category-items">
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Equipments</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Laboratory Setup</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Reagents and Kits</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>OT accessories</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Surgical Items  </span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>First Aid</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Opthalmic products</span>
+                </div>
+                <div className="item">
+                    <BsFillMortarboardFill /> <span>Sales and Service</span>
+                </div>
+              </div>
+                 
+            </div>
+        </div>
+        <div className="hero-right">
+            <div className="hero-image-container">
+              {/* <img src={hero_bg} alt="" />
+              <img src={hero_bg2} alt="" />
+              <img src={hero_bg} alt="" /> */}
+
+              <Swiper
+        spaceBetween={30}
+        effect={'fade'}
+        // navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={hero_bg} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={hero_bg2} />
+        </SwiperSlide>
+
+      </Swiper>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Hero } from '../../Components/Hero/Hero'
 
 
 export const Home = () => {
   return (
     <div>
-        
+        <Hero></Hero>
     </div>
   )
 }
